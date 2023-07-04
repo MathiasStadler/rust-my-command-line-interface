a204f7cce5c397fd5937607ccb33081031d0d3a5
dudoof <email@example.com>
2023-07-04 11:28:57 +0200

    init next step

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# from here
+# https://github.com/kyclark/command-line-rust/blob/main/test.sh
+
+for DIR in [a01]*; do
+    DIRNAME=$(basename "$DIR")
+    echo "==> $DIRNAME <=="
+    (cd $DIR && cargo test -q > /dev/null && cargo clippy)
+done
+
+echo "Done."
