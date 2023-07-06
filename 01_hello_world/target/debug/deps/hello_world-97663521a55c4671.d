ffe992df091a580cce828bdc277cc2224029c873
dudoof <email@example.com>
2023-07-06 11:54:26 +0200

    Removed  target folder

--- 01_hello_world/target/debug/deps/hello_world-97663521a55c4671.d
+++
@@ -1,5 +0,0 @@
-/home/trapapa/rust-my-command-line-interface/01_hello_world/target/debug/deps/hello_world-97663521a55c4671: src/main.rs
-
-/home/trapapa/rust-my-command-line-interface/01_hello_world/target/debug/deps/hello_world-97663521a55c4671.d: src/main.rs
-
-src/main.rs:
